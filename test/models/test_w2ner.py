41e76d1a8bb30f46724164c66db868cc638f292a
HongbangYuan <1208314139@qq.com>
2022-04-07 20:50:30 +0800

    add some w2ner module

--- test/models/test_w2ner.py
+++ test/models/test_w2ner.py
@@ -5,6 +5,9 @@ import torch.optim as optim
 from torch.utils.data import RandomSampler
 from cogie.io.loader.ner.conll2003 import Conll2003NERLoader
 from cogie.io.processor.ner.conll2003 import Conll2003NERProcessor,Conll2003W2NERProcessor
+from cogie.models.ner.w2ner import W2NER
+import json
+from argparse import Namespace
 
 
 device = torch.device('cuda')
@@ -25,5 +28,52 @@ train_sampler = RandomSampler(train_dataset)
 # test_datable = processor.process(test_data)
 # test_dataset = cogie.DataTableSet(test_datable)
 # test_sampler = RandomSampler(test_dataset)
+
+with open("./conll03.json","r") as f:
+    config = json.load(f)
+config = Namespace(**config)
+config.label_num = len(vocabulary.word2idx)
+config.vocab = vocabulary
+model = W2NER(config)
+
+metric = cogie.SpanFPreRecMetric(vocabulary)
+loss = nn.CrossEntropyLoss(ignore_index=0)
+optimizer = optim.Adam(model.parameters(), lr=0.000005)
+trainer = cogie.Trainer(model,
+                        train_dataset,
+                        dev_data=None,
+                        n_epochs=2,
+                        batch_size=20,
+                        loss=loss,
+                        optimizer=optimizer,
+                        scheduler=None,
+                        metrics=metric,
+                        train_sampler=train_sampler,
+                        dev_sampler=None,
+                        drop_last=False,
+                        gradient_accumulation_steps=1,
+                        num_workers=5,
+                        save_path="../../../cognlp/data/ner/conll2003/model",
+                        save_file=None,
+                        print_every=None,
+                        scheduler_steps=None,
+                        validate_steps=100,
+                        save_steps=None,
+                        grad_norm=None,
+                        use_tqdm=True,
+                        device=device,
+                        device_ids=[0],
+                        callbacks=None,
+                        metric_key=None,
+                        writer_path='../../../cognlp/data/ner/conll2003/tensorboard',
+                        fp16=False,
+                        fp16_opt_level='O1',
+                        checkpoint_path=None,
+                        task='conll2003',
+                        logger_path='../../../cognlp/data/ner/conll2003/logger')
+
+trainer.train()
+
+
 
 
