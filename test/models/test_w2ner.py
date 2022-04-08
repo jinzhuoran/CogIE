7e49c7d40daad5c78dd0e08dcd0b1eaad551bf58
HongbangYuan <1208314139@qq.com>
2022-04-08 12:10:05 +0800

    modify parameters using json

--- test/models/test_w2ner.py
+++ test/models/test_w2ner.py
@@ -8,6 +8,7 @@ from cogie.io.processor.ner.conll2003 import Conll2003NERProcessor,Conll2003W2NE
 from cogie.models.ner.w2ner import W2NER
 import json
 from argparse import Namespace
+import transformers
 
 
 device = torch.device('cuda')
@@ -36,14 +37,35 @@ config.label_num = len(vocabulary.word2idx)
 config.vocab = vocabulary
 model = W2NER(config)
 
+bert_params = set(model.bert.parameters())
+other_params = list(set(model.parameters()) - bert_params)
+no_decay = ['bias', 'LayerNorm.weight']
+params = [
+    {'params': [p for n, p in model.bert.named_parameters() if not any(nd in n for nd in no_decay)],
+     'lr': config.bert_learning_rate,
+     'weight_decay': config.weight_decay},
+    {'params': [p for n, p in model.bert.named_parameters() if any(nd in n for nd in no_decay)],
+     'lr': config.bert_learning_rate,
+     'weight_decay': 0.0},
+    {'params': other_params,
+     'lr': config.learning_rate,
+     'weight_decay': config.weight_decay},
+]
+
+updates_total = len(train_dataset[0]) // config.batch_size * config.epochs
+optimizer = transformers.AdamW(params, lr=config.learning_rate, weight_decay=config.weight_decay)
+scheduler = transformers.get_linear_schedule_with_warmup(optimizer,
+                                                              num_warmup_steps=config.warm_factor * updates_total,
+                                                              num_training_steps=updates_total)
+
 metric = cogie.SpanFPreRecMetric(vocabulary)
 loss = nn.CrossEntropyLoss(ignore_index=0)
-optimizer = optim.Adam(model.parameters(), lr=0.000005)
+# optimizer = optim.Adam(model.parameters(), lr=1e-3)
 trainer = cogie.Trainer(model,
                         train_dataset,
                         dev_data=dev_dataset,
-                        n_epochs=2,
-                        batch_size=5,
+                        n_epochs=config.epochs,
+                        batch_size=config.batch_size,
                         loss=loss,
                         optimizer=optimizer,
                         scheduler=None,
@@ -57,7 +79,7 @@ trainer = cogie.Trainer(model,
                         save_file=None,
                         print_every=None,
                         scheduler_steps=None,
-                        validate_steps=500,
+                        validate_steps=1000,
                         save_steps=None,
                         grad_norm=None,
                         use_tqdm=True,
